cc89605ae75a35f3b91cc117807c9ce68392fcdd
Luis Pinto <dapin46@hotmail.com>
2025-09-12 10:33:54 -0500

    Update: email removed in contact-form-inputs.ts

--- src/types/contact-form-inputs.ts
+++ src/types/contact-form-inputs.ts
@@ -3,7 +3,6 @@ import type { InterestDentistryService } from './interest-dentistry-service';
 export type ContactFormInputs = {
   name: string;
   lastName: string;
-  email: string;
   service: InterestDentistryService;
   message: string;
 };

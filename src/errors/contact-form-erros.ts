97846ef68f5ddff42de9ed047b55a498bfc34bd8
Luis Pinto <dapin46@hotmail.com>
2025-09-12 11:42:51 -0500

    Add: errors for ContactForm created

---
+++ src/errors/contact-form-erros.ts
@@ -0,0 +1,15 @@
+export const contactFormErrors = {
+  name: {
+    required: 'Por favor digite su nombre',
+    minLength: 'Digite al menos 2 carácteres',
+    maxLength: 'Hay más de 50 carácteres',
+  },
+
+  lastName: {
+    required: 'Por favor digite su nombre',
+    minLength: 'Digite al menos 2 carácteres',
+    maxLength: 'Hay más de 50 carácteres',
+  },
+
+  service: { required: 'Elija el servicio que quiere recibir' },
+};

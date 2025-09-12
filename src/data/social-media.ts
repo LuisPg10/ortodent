ec848fdfdf0e6041d41babdc10cf022fb8ba5f17
Luis Pinto <dapin46@hotmail.com>
2025-09-12 15:25:50 -0500

    Update: Link to instragram changed

--- src/data/social-media.ts
+++ src/data/social-media.ts
@@ -1,6 +1,5 @@
 import type { LinkItem } from '@/types/link-item';
 
 export const socialNetwork: LinkItem[] = [
-  { title: 'Facebook', link: 'https://www.facebook.com/' },
-  { title: 'Instagram', link: 'https://www.instagram.com/' },
+  { title: 'Instagram', link: 'https://www.instagram.com/ortodent_' },
 ];

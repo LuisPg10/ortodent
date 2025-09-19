2a5e4e9c056cb742b0c7afe663b5353c4abc4376
Luis Pinto <dapin46@hotmail.com>
2025-09-19 14:09:18 -0500

    Add: Ortodent logo added to the header

--- src/components/ui/HeaderContent.tsx
+++ src/components/ui/HeaderContent.tsx
@@ -1,6 +1,5 @@
-import { Smile } from 'lucide-react';
-
 import { navContent } from '@/data/nav-content';
+import logoOrtodent from '../../assets/images/logo-ortodent.webp';
 
 import { Button } from './button';
 import { CustomList } from './CustomList';
@@ -8,9 +7,9 @@ import { CustomList } from './CustomList';
 export const HeaderContent = () => {
   return (
     <div className="container mx-auto flex items-center justify-between px-4 py-4">
-      <div className="flex items-center space-x-2">
-        <div className="bg-primary flex h-8 w-8 items-center justify-center rounded-full">
-          <Smile className="text-primary-foreground h-5 w-5" />
+      <div className="flex items-center space-x-1">
+        <div className="flex h-8 w-8 items-center justify-center rounded-full">
+          <img src={logoOrtodent} alt="Logo de la empresa Ortodent" />
         </div>
 
         <h1 className="text-foreground font-serif text-xl font-bold">
